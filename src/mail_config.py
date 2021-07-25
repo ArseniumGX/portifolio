6896a645755fca58d9719da97df90119c521ab10
José Pereira Macedo <arseniumgx@gmail.com>
2021-07-25 20:59:55 -0300

    flask-mail config file

---
+++ src/mail_config.py
@@ -0,0 +1,8 @@
+config = {
+    'MAIL_SERVER': 'smtp.mailtrap.io',
+    'MAIL_PORT': 2525,
+    'MAIL_USERNAME': '5622d2e98706a9',
+    'MAIL_PASSWORD': 'f296da83b42768',
+    'MAIL_USE_TLS': True,
+    'MAIL_USE_SSL': False
+}
